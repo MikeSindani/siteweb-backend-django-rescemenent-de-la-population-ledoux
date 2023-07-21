aef809d5558dd5968a5ae1cd24bc0ce3665e6efb
MikeSindani <mikems@live.fr>
2023-07-21 23:08:35 +0200

    correct bug zone

--- Zone_APP/views.py
+++ Zone_APP/views.py
@@ -11,7 +11,12 @@ def nom_de_la_zone(variable__nombres__totals__des__zones):
      return  "ZD-"
 # Create your views here.
 def verifier__nom__zones(request):
-      variable__nombres__totals__des__zones  = Zones.objects.latest('id').id
+      variable__nombres__totals__des__zones  = 0
+      try:
+          variable__nombres__totals__des__zones  = Zones.objects.latest('id').id
+      except:
+          variable__nombres__totals__des__zones  = 0
+      
       variable__nombres__totals__des__zones = variable__nombres__totals__des__zones + 1
       variable__nom__de__la__zones = nom_de_la_zone(variable__nombres__totals__des__zones)+str(variable__nombres__totals__des__zones)   
      
@@ -27,7 +32,12 @@ def creation__zones(request):
         nom__de__zone = request.POST['nom_de_zone']
         print('+'*20)
         print(nom__de__zone)
-        variable__nombres__totals__des__zones  = Zones.objects.latest('id').id
+        variable__nombres__totals__des__zones  = 0
+        try:
+            variable__nombres__totals__des__zones  = Zones.objects.latest('id').id
+        except:
+            variable__nombres__totals__des__zones  = 0
+            
         variable__nombres__totals__des__zones = variable__nombres__totals__des__zones + 1
         variable__nom__de__la__zones = nom_de_la_zone(variable__nombres__totals__des__zones)+str(variable__nombres__totals__des__zones) 
         z = Zones(nom=variable__nom__de__la__zones,etat=0)
